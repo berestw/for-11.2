7741bbc912536ac059714f93db3408a60fb7c778
berestw <paladin.berestw@yandex.ru>
2024-01-05 19:27:38 +0300

    nado bolshe bukavi 2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
-stroka = input()
-print(stroka.upper())
+def get_upper(text):
+    """function for upper symbols"""
+    return text.upper()
+
+
+def get_title(text):
+    """function for title symbols"""
+    return text.title()
